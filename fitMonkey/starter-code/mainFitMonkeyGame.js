2654063255f44dac0e956cb5cd57cef10f9f47a7
Juan C. Rodriguez <rodriguez.juan.c@gmail.com>
2017-04-15 12:46:35 +0200

    added initial load sound and logo

--- fitMonkey/starter-code/mainFitMonkeyGame.js
+++ fitMonkey/starter-code/mainFitMonkeyGame.js
@@ -1,5 +1,26 @@
 $(document).ready(function() {
 
+  // var audio = document.createElement("audio");
+  // audio.src = "../sounds/circus-sound.mp3";
+  //
+  // audio.addEventListener("canplaythrough", function () {
+  //         setTimeout(function(){
+  //             audio.pause();
+  //         },
+  //         10000);
+  // }, false);
+
+  window.onload = function() {
+      var audioEl = document.getElementById("circus-sound");
+      audioEl.load();
+      audioEl.play();
+      audioEl.addEventListener("canplaythrough", function () {
+              setTimeout(function(){
+                  audioEl.pause();
+              },
+              3500);
+      }, false);
+  };
 
 
 
