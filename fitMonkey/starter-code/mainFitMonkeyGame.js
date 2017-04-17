59be704a4d41a5cee057edf576b95b40566a4f4b
Juan C. Rodriguez <rodriguez.juan.c@gmail.com>
2017-04-17 11:06:20 +0200

    added item object

--- fitMonkey/starter-code/mainFitMonkeyGame.js
+++ fitMonkey/starter-code/mainFitMonkeyGame.js
@@ -1,26 +1,39 @@
+function Game() {
+  this.shootingRows = [];
+  this.items = [
+    {name: 'banana', type: 'fruit', image: 'banana.gif'},
+    {name: 'apple', type: 'fruit', image: 'apple.jpeg'},
+    {name: 'coke', type: 'dessert', image: 'coke.jpg'},
+    {name: 'cookie', type: 'dessert', image: 'cookie.jpg'},
+    {name: 'cupcake', type: 'dessert', image: 'cupcake.jpeg'},
+    {name: 'grape', type: 'fruit', image: 'grape.jpg'},
+    {name: 'trash', type: 'trash', image: 'trash-bin.jpg'}
+  ]
+
+}
+
+
+
+
+
+
+
+window.onload = function() {
+    var audioEl = document.getElementById("circus-sound");
+    audioEl.load();
+    audioEl.play();
+    audioEl.addEventListener("canplaythrough", function () {
+            setTimeout(function(){
+                audioEl.pause();
+            },
+            3500);
+    }, false);
+};
+
+
+
+
 $(document).ready(function() {
-
-  // var audio = document.createElement("audio");
-  // audio.src = "../sounds/circus-sound.mp3";
-  //
-  // audio.addEventListener("canplaythrough", function () {
-  //         setTimeout(function(){
-  //             audio.pause();
-  //         },
-  //         10000);
-  // }, false);
-
-  window.onload = function() {
-      var audioEl = document.getElementById("circus-sound");
-      audioEl.load();
-      audioEl.play();
-      audioEl.addEventListener("canplaythrough", function () {
-              setTimeout(function(){
-                  audioEl.pause();
-              },
-              3500);
-      }, false);
-  };
 
 
 
