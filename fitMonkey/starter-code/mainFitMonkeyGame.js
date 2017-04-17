cef12e900e00eda7ee94ba7a460ed9d56b84bee9
Juan C. Rodriguez <rodriguez.juan.c@gmail.com>
2017-04-17 16:18:53 +0200

    added wood deck, shooting lines, and shooting background

--- fitMonkey/starter-code/mainFitMonkeyGame.js
+++ fitMonkey/starter-code/mainFitMonkeyGame.js
@@ -1,5 +1,8 @@
 function Game() {
-  this.shootingRows = [];
+  this.shootingRow1 = [];
+  this.shootingRows = ['first','second','third'];
+  this.shootingColumns = 15;
+  this.gameEnd = false;
   this.items = [
     {name: 'banana', type: 'fruit', image: 'banana.gif'},
     {name: 'apple', type: 'fruit', image: 'apple.jpeg'},
@@ -8,33 +11,64 @@ function Game() {
     {name: 'cupcake', type: 'dessert', image: 'cupcake.jpeg'},
     {name: 'grape', type: 'fruit', image: 'grape.jpg'},
     {name: 'trash', type: 'trash', image: 'trash-bin.jpg'}
-  ]
+  ];
 
+  for(var rowIndex = 0; rowIndex < this.shootingRows.length; rowIndex++) {
+    for(var columnIndex = 0; columnIndex < this.shootingColumns; columnIndex++) {
+      var temp = this.shootingRows[rowIndex];
+      console.log(temp);
+      $('.' + temp).append( $('<div>')
+        .addClass('shooting-row')
+        .attr('row-num', rowIndex)
+        .attr('col-num', columnIndex));
+  }
+
+  $('.header-text').show(5000);
+
+  var audioEl = document.getElementById("circus-sound");
+  audioEl.load();
+  audioEl.play();
+  audioEl.addEventListener("canplaythrough", function () {
+          setTimeout(function(){
+              audioEl.pause();
+          },
+          3500);
+  }, false);
+}
 }
 
-
-
-
-
-
-
-window.onload = function() {
-    var audioEl = document.getElementById("circus-sound");
-    audioEl.load();
-    audioEl.play();
-    audioEl.addEventListener("canplaythrough", function () {
-            setTimeout(function(){
-                audioEl.pause();
-            },
-            3500);
-    }, false);
+Game.prototype.addToRow = function() {
+  for (i = 15; i >= 0; i -= 2) {
+    var randomNum = Math.floor(Math.random() * this.items.length);
+    console.log(randomNum);
+    this.shootingRow1.push(this.items[randomNum].name);
+    this.shootingRow1.push('blank');
+    console.log(this.items[randomNum].name);
+  }
 };
+
+Game.prototype.makeRowMove = function () {
+  while(this.gameEnd) {
+    var randomNum = Math.floor(Math.random() * this.items.length);
+    this.shootingRow1.splice(1,0);
+    this.shootingRow1.push(this.items[randomNum].name);
+    this.shootingRow1.splice(1,0);
+    this.shootingRow1.push('blank');
+    console.log(this.shootingRow1);
+
+  }
+};
+
 
 
 
 
 $(document).ready(function() {
 
+  var newGame = new Game();
+  newGame.addToRow();
 
+  // newGame.makeRowMove();
+  // console.log(newGame.shootingRow1);
 
 });
